2378e158bceb5a6170004d9506f323b0d4ed36d0
mateus-sposo <mat.sposo@gmail.com>
2023-06-25 06:00:29 -0300

    _

--- Beecrowd/lista de treinamento final/2854.py
+++ Beecrowd/lista de treinamento final/2854.py
@@ -8,10 +8,22 @@ for i in range(N-1):
     n1, _, n2 = input().split()
     Matriz.append([n1, n2])
 
+def check(Matriz, Fam, C, c, Total):
+    flag = True
+    for j in range(c):
+        if Matriz[i][0] in Fam[j] or Matriz[i][1] in Fam[j]:
+            flag = False
+            break
+    if flag:
+        Fam.append([Matriz[i][0], Matriz[i][1]])
+        del(Matriz[i])
+        C += 1
+        Total += 2
+    return Matriz, Fam, C, Total
 
 Total = 2
 c = 0
-
+C = 0
 while True:
     for i in range(len(Fam[c])):
         j = 0
@@ -29,27 +41,21 @@ while True:
                 Total += 1
                 del(Matriz[j])
                 delete = True
+            elif Matriz[j][0] in Fam[c]:
+                del(Matriz[j])
+                delete = True
+            elif C == c:
+                Matriz, Fam, C, Total = check(Matriz, Fam, C, c, Total)
             if Total >= M:
                 break
             if not delete:
                 j += 1
-
+        if Total >= M:
+            break
+    c = int(C)
+    
     if Total >= M:
         break
-
-    for i in range(len(Matriz)):
-        if Matriz[i][0] not in Fam[c] and Matriz[i][1] not in Fam[c]:
-            flag = True
-            for j in range(c):
-                if Matriz[i][0] in Fam[j] or Matriz[i][1] in Fam[j]:
-                    flag = False
-                    break
-            if flag:
-                Fam.append([Matriz[i][0], Matriz[i][1]])
-                del(Matriz[i])
-                c += 1
-                Total += 2
-                break
 
 print(Fam, c+1)
 
--- Beecrowd/lista de treinamento final/3050.py
+++ Beecrowd/lista de treinamento final/3050.py
@@ -1,2 +1,10 @@
+#incompleto
 N = int(input())
-vetor = [int(a) for a in input().split()]
+vetor = input().split()
+dist_max = 0
+
+i = 0
+j = N-1
+
+
+print(dist_max)
