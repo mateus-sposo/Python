76841e56ea84f31b38dafe92f8a11bee5bdd7b00
mateus-sposo <mat.sposo@gmail.com>
2023-06-26 03:18:25 -0300

    _

--- Beecrowd/lista de treinamento final/3050.py
+++ Beecrowd/lista de treinamento final/3050.py
@@ -1,10 +1,17 @@
+#incompleto
+
 N = int(input())
-vetor = input().split()
+vetor = [int(a) for a in input().split()]
 dist_max = 0
+maior = 0
 
 for i in range(N):
-    for j in range(i+1, N):
-        dist = int(vetor[i]) + int(vetor[j]) + (j-i)
-        if dist > dist_max:
-            dist_max = dist
+    if vetor[i] > maior:
+        maior = vetor[i]
+        p = i
+
+for i in range(N):
+    if maior + vetor[i] + abs(p-i) > dist_max:
+        dist_max = maior + vetor[i] + abs(p-i)
+
 print(dist_max)
