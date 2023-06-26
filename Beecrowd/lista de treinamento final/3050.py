a99bbe6c5650fb02898ef27e566a91e105bae84a
mateus-sposo <mat.sposo@gmail.com>
2023-06-26 16:53:57 -0300

    3050

--- Beecrowd/lista de treinamento final/3050.py
+++ Beecrowd/lista de treinamento final/3050.py
@@ -6,12 +6,12 @@ dist_max = 0
 maior = 0
 
 for i in range(N):
-    if vetor[i] > maior:
-        maior = vetor[i]
+    if vetor[0] + vetor[i] + i > maior:
+        maior = vetor[i] + vetor[0] + i
         p = i
 
 for i in range(N):
-    if maior + vetor[i] + abs(p-i) > dist_max:
-        dist_max = maior + vetor[i] + abs(p-i)
+    if vetor[p] + vetor[i] + abs(p-i) > dist_max:
+        dist_max = vetor[p] + vetor[i] + abs(p-i)
 
 print(dist_max)
