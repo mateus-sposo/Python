15e50e07b5b743bf3fdda658ab0b33756d688aa3
Mateus de Oliveira Sposo <mat.sposo@gmail.com>
2023-06-29 22:36:18 -0300

    _

--- Exercícios/AVA/Exercícios vetores-matrizes/Ex_1.py
+++ Exercícios/AVA/Exercícios vetores-matrizes/Ex_1.py
@@ -1,7 +1,9 @@
 #impressão inversa de um vetor
 
 vetor = [int(a) for a in input().split()]
+# 1 2 3 4 5 6
 
 vetorf = [vetor[i] for i in range(len(vetor) -1, -1, -1)]
+#[5, -1, -1]
 
 print(vetorf)
